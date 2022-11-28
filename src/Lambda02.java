420e722ffcb23dd12a0bb730a968d3f486794520
feyza-unal <feyzaunal95@gmail.com>
2022-11-28 15:51:36 +0300

    lambda D2

--- src/Lambda02.java
+++ src/Lambda02.java
@@ -20,6 +20,14 @@ public class Lambda02 {
         ciftKareEnBuyukPrint(sayi);
         System.out.println();
         elemanlarinToplamiPrint(sayi);
+        System.out.println();
+        ciftCarpimPrint(sayi);
+        System.out.println();
+        minBul(sayi);
+        System.out.println();
+        bestenBuyukMinTek(sayi);
+        System.out.println();
+        ciftKareSirala(sayi);
     }
 
 //---------------------------------------------------------------------------------------------------------
@@ -28,7 +36,7 @@ public class Lambda02 {
         sayi.
                 stream().
                 filter(Lambda01::ciftBul). // streamdeki(akis) cift sayilari filtreledim 4 2 6
-                map(t->t*t).  // 16 4 36  --> stream icerisindeki elemanlari baska degerlere donusturur
+                map(t->t*t).  // 16 4 36  -->map() stream icerisindeki elemanlari baska degerlere donusturur
                 forEach(Lambda01::yazdir);
     }
 
@@ -55,9 +63,11 @@ public class Lambda02 {
 //---------------------------------------------------------------------------------------------------------
 // Task-4 : list'in en buyuk elemanini yazdiriniz
     public static void enBuyukPrint(List<Integer> sayi){
-        Optional<Integer> maxSayi = sayi.
+        Optional<Integer> maxSayi = sayi.  // optional-> reduce() methodu ile null p.exc. gelirse kod calismayi durdurur.
+                // bunu onlemek icin optional kullanilir
                 stream().
-                reduce(Math::max); // eger sonuc tek bir deger ise o zaman reduce() terminal operatoru kullanilir
+                reduce(Math::max); // eger sonuc tek bir deger ise o zaman reduce() terminal operatoru kullanilir. terminal
+        // operatoru oldugu icin akis biter
         System.out.println(maxSayi); //Optional[15]
     }
 
@@ -74,30 +84,77 @@ public class Lambda02 {
 //---------------------------------------------------------------------------------------------------------
 // Task-5 : List'in cift elemanlarin karelerinin en buyugunu print ediniz
     public static void ciftKareEnBuyukPrint(List<Integer> sayi){
-        Optional<Integer> max = sayi.stream().
+        System.out.println(sayi.stream().  // atama yapmadan direk yazdirdigimiz icin optional kullanmadik
                 filter(Lambda01::ciftBul).
-                map(a->a*a).
-                reduce(Integer::max);  //Math::max a gore daha hizli calisir
-        System.out.println("max = " + max); //max = Optional[36]
+                map(a -> a * a).
+                reduce(Integer::max));  // "Integer::max" methodu, "Math::max"e gore daha hizli calisir.(Math class cok buyuk)
     }
 
 //---------------------------------------------------------------------------------------------------------
 // Task-6: List'teki tum elemanlarin toplamini yazdiriniz.Lambda Expression...
-
     public static void elemanlarinToplamiPrint(List<Integer> sayi){
         int toplam = sayi.
                 stream().
                 reduce(0,(a,b) -> a+b);
         System.out.println("toplam = " + toplam); //toplam = 43
+    }
+/*
+    * a ilk degerini her zaman atanan degerden (ilk parametre) alır, bu örnekte 0 oluyor (sum=0 gibi etkisiz eleman atiyoruz)
+    * b degerini her zamana  stream()'dan akısdan alır
+    * a ilk degerinden sonraki her değeri action(işlem)'dan alır
+*/
+//---------------------------------------------------------------------------------------------------------
+// Task-7 : List'teki cift elemanlarin carpimini  yazdiriniz.
+    public static void ciftCarpimPrint(List<Integer> sayi){
+        System.out.println(sayi.
+                stream().
+                filter(Lambda01::ciftBul).
+                reduce(Math::multiplyExact)); //48      => method reference
+        // Math::multiplyExact-> listedeki butun elemanlari carpip gonderir
 
+        System.out.println(sayi.
+                stream().
+                filter(Lambda01::ciftBul).
+                reduce(1, (a, b) -> a * b));  //   => lambda expression
+    }
+
+//---------------------------------------------------------------------------------------------------------
+// Task-8 : List'teki elemanlardan en kucugunu  print ediniz.
+    public static void minBul(List<Integer> sayi){
+        //1. yol method reference
+        System.out.println(sayi.
+                stream().
+                reduce(Integer::min));  // Math::min
+
+        //2. yol lambda expression
+        System.out.println(sayi.
+                stream().
+                reduce(Lambda02::bulMin));
+    }
+    public static int bulMin(int a , int b) {
+
+        return (a<b) ? a : b ;   // byMiracMin(4,9)
+    }
+
+//---------------------------------------------------------------------------------------------------------
+// Task-9 : List'teki 5'ten buyuk en kucuk tek sayiyi print ediniz.
+    public static void bestenBuyukMinTek(List<Integer> sayi){
+        System.out.println(sayi.
+                stream().
+                filter(t -> (t > 5) && (t % 2 == 1)).
+                reduce(Lambda02::bulMin));
     }
 
 
-
-
-
-
-
-
+//---------------------------------------------------------------------------------------------------------
+// Task-10 : list'in cift  elemanlarinin karelerini  kucukten buyuge print ediniz.
+    public static void ciftKareSirala(List<Integer> sayi){
+        sayi.
+                stream().
+                filter(Lambda01::ciftBul).
+                map(t->t*t).// sayıların karesi ile yeni bir akış oluşturdum
+                sorted(). // akış içindeki sayıları natural-order olarak sıraladım
+                forEach(Lambda01::yazdir); // akışdaki, sayıları ekrana yazdım
+    }
 }
 
